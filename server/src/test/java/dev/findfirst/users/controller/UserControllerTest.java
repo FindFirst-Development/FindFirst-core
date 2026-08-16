2e8957dbd73d4cb857d31f3dac374652c82931ca
Figes <saishivapacha@gmail.com>
2026-08-16 14:59:44 +0530

    issue-137-User-Deletion-feature (#422)
    * issue-137-User-Deletion-feature
    
    * Implemented User Deletion feature
    
    * added second test and removed null ambiguity of wrapper classes

--- server/src/test/java/dev/findfirst/users/controller/UserControllerTest.java
+++ server/src/test/java/dev/findfirst/users/controller/UserControllerTest.java
@@ -2,7 +2,9 @@ package dev.findfirst.users.controller;
 
 import static dev.findfirst.utilities.HttpUtility.getHttpEntity;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.nio.file.Files;
@@ -12,12 +14,17 @@ import java.util.Properties;
 
 import dev.findfirst.core.annotations.IntegrationTest;
 import dev.findfirst.core.annotations.MockTypesense;
+import dev.findfirst.core.repository.jdbc.BookmarkJDBCRepository;
+import dev.findfirst.core.repository.jdbc.TagJDBCRepository;
 import dev.findfirst.core.service.TypesenseService;
 import dev.findfirst.security.userauth.models.TokenRefreshResponse;
 import dev.findfirst.security.userauth.models.payload.request.SignupRequest;
+import dev.findfirst.users.exceptions.NoUserFoundException;
 import dev.findfirst.users.model.MailHogMessage;
 import dev.findfirst.users.model.oauth2.Oauth2Source;
 import dev.findfirst.users.model.user.TokenPassword;
+import dev.findfirst.users.model.user.User;
+import dev.findfirst.users.repository.UserRepo;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Order;
@@ -30,15 +37,12 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+import org.springframework.http.*;
 import org.springframework.http.client.MultipartBodyBuilder;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.web.client.HttpClientErrorException;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
@@ -133,6 +137,7 @@ class UserControllerTest {
       assertEquals(HttpStatus.SEE_OTHER, regResponse.getStatusCode());
     } catch (Exception e) {
       // fail the test should show message
+
       assertTrue(false, e.getMessage());
     }
   }
@@ -249,5 +254,60 @@ class UserControllerTest {
     var sources = response.getBody();
     assertTrue(sources.length == 1);
     assertEquals("GitHub", sources[0].provider(), "Github should be the provider.");
+  }
+
+  @Autowired
+  private UserRepo userRepo;
+
+  @Autowired
+  private BookmarkJDBCRepository bookmarkJDBCRepository;
+
+  @Autowired
+  private TagJDBCRepository tagJDBCRepository;
+
+  @Test
+  void testDeleteUser() throws Exception {
+
+    SignupRequest request = new SignupRequest("testUser", "testUser@gmail.com", "testPassword");
+
+    restTemplate.postForEntity("/user/signup", request, String.class);
+
+    var token = getTokenFromEmail(0, 1);
+
+    var regResponse =
+        restTemplate.getForEntity("/user/regitrationConfirm?token={token}", String.class, token);
+    assertEquals(HttpStatus.SEE_OTHER, regResponse.getStatusCode());
+
+    // Sign in
+    HttpHeaders signInHeaders = new HttpHeaders();
+    signInHeaders.setBasicAuth("testUser", "testPassword");
+    HttpEntity<String> signInEntity = new HttpEntity<>(signInHeaders);
+    var signInResponse = restTemplate.exchange("/user/signin", HttpMethod.POST, signInEntity,
+        TokenRefreshResponse.class);
+    assertEquals(HttpStatus.OK, signInResponse.getStatusCode());
+
+    User user = userRepo.findByUsername("testUser").orElseThrow(NoUserFoundException::new);
+
+    // first delete
+    HttpHeaders headers = new HttpHeaders();
+    String accessToken = signInResponse.getBody().accessToken();
+    headers.setBearerAuth(accessToken);
+    HttpEntity<String> entity = new HttpEntity<>(headers);
+    var response1 = restTemplate.exchange("/user", HttpMethod.DELETE, entity, Void.class);
+    assertEquals(HttpStatus.OK, response1.getStatusCode());
+
+    // Checking
+
+    assertFalse(userRepo.existsByUsername("testUser"));
+    assertTrue(bookmarkJDBCRepository.findAllBookmarksByUser(user.getUserId()).isEmpty());
+    assertTrue(tagJDBCRepository.findAllByUserId(user.getUserId()).isEmpty());
+
+    // second delete should throw exception of not found as we are not revoking the token
+    ResponseEntity<Void> response =
+            restTemplate.exchange("/user", HttpMethod.DELETE, entity, Void.class);
+
+    System.out.println(response.getStatusCode());
+
+    assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
   }
 }
