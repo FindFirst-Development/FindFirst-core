0748de84ce8ef12b6cff5ac66df138325fad6687
Raphael J. Sandor <45048465+R-Sandor@users.noreply.github.com>
2026-08-26 05:47:42 -0400

    planning

---
+++ frontend/services/view.service.tsx
@@ -0,0 +1,15 @@
+
+/**
+  * Designed to be the query/state manager.
+  * A feature complete ViewService should be able
+  * to handle the bounding box dimensions
+  * and resize events.
+  *
+  *
+  */
+class ViewService {
+
+
+
+
+}
